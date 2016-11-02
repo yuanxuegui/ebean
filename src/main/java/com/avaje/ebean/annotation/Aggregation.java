451b037b3fdd4ae90ceff164ec55b7b3058f0030
Rob Bygrave <robin.bygrave@gmail.com>
2016-11-02 21:59:11 +1300

    Update javadoc for @Aggregation

--- src/main/java/com/avaje/ebean/annotation/Aggregation.java
+++ src/main/java/com/avaje/ebean/annotation/Aggregation.java
@@ -8,6 +8,10 @@ import java.lang.annotation.Target;
 /**
  * Specify a property to be an aggregation formula.
  * <p>
+ * The aggregation formula should be a sum, count, avg, min or max.
+ * By default aggregation properties are treated as transient and not
+ * included in a query. To populate the aggregation property it must be
+ * explicitly included in the select().
  * </p>
  *
  * <h3>Example:</h3>
@@ -18,6 +22,20 @@ import java.lang.annotation.Target;
  *
  * @Aggregation("sum(details.quantity*details.unitPrice)")
  * Long totalAmount;
+ *
+ * }</pre>
+ *
+ * <h3>Example query</h3>
+ * <pre>{@code
+ *
+ *  List<TEventOne> list = Ebean.find(TEventOne.class)
+ *       .select("name, totalCount, totalUnits, totalAmount")
+ *       .where()
+ *         .startsWith("logs.description", "a")
+ *       .having()
+ *         .ge("count", 1)
+ *       .orderBy().asc("name")
+ *       .findList();
  *
  * }</pre>
  */
